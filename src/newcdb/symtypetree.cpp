b4fe30d78b743cecd2a246c02b486131bc31874f
rickyw <rickyw@07914c0c-2d3e-0410-adf3-90c1a7ff5250>
2007-03-26 09:41:39 +0000

    add files needed for variable printing
    git-svn-id: https://svn.code.sf.net/p/ec2drv/code/ec2drv/trunk@228 07914c0c-2d3e-0410-adf3-90c1a7ff5250

---
+++ src/newcdb/symtypetree.cpp
@@ -0,0 +1,343 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Ricky White   *
+ *   rickyw@neatstuff.co.nz   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include <iostream>
+#include <iomanip>
+#include "symtypetree.h"
+#include "contextmgr.h"
+#include "target.h"
+#include "memremap.h"
+#include "outformat.h"
+extern Target *target;
+
+using namespace std;
+
+SymTypeTree sym_type_tree;		// singleton object
+
+
+SymTypeTree::SymTypeTree()
+{
+	// Add terminal types to the tree
+	m_types.push_back(new SymTypeChar());
+	m_types.push_back(new SymTypeUChar());
+	m_types.push_back(new SymTypeShort());
+	m_types.push_back(new SymTypeUShort());
+	m_types.push_back(new SymTypeInt());
+	m_types.push_back(new SymTypeUInt());
+	m_types.push_back(new SymTypeFloat());
+	m_types.push_back(new SymTypeSbit());
+	/*	
+	SymTypeStruct *a = new SymTypeStruct();
+	a->set_name("test_1");
+	m_types.push_back(a);
+	a = new SymTypeStruct();
+	a->set_name("astruct");
+	m_types.push_back(a);
+	
+	a->add_member( "q", i );
+	a->add_member( "r", i );
+	a->add_member( "s", i );
+	*/
+}
+
+
+SymTypeTree::~SymTypeTree()
+{
+}
+
+
+bool SymTypeTree::add_type( SymType *ptype )
+{
+	m_types.push_back(ptype);
+}
+
+void SymTypeTree::dump()
+{
+	cout << setw(24) << left <<"Type name"
+		<< setw(9)<< left << "Terminal"
+		<< setw(8)<< left << "Size"
+		<< setw(24)<< left << "Scope"
+		<< endl;
+	cout << string(80,'=') << endl;
+	for( int i=0; i<m_types.size(); i++ )
+	{
+		cout << setw(24) << left << m_types[i]->name()
+			<< setw(9) << left << boolalpha << m_types[i]->terminal()
+			<< setw(8) << left <<  m_types[i]->size()
+			<< setw(24) << left <<  m_types[i]->file()
+			<< endl;
+	}
+	cout << endl;
+}
+
+
+void SymTypeTree::dump(std::string type_name)
+{
+	// find type
+	for(int i=0; i<m_types.size(); i++)
+	{
+		cout << "checking "<<m_types[i]->name()<<endl;
+		if(m_types[i]->name()==type_name)
+		{
+			cout << "Dumping type = '"<<type_name<<"'"<<endl;
+			cout << m_types[i]->text() << endl;
+			return;
+		}
+	}
+	cout << "ERROR Type = '"<<type_name<<"' not found."<<endl;
+}
+
+#if 0
+/** scan for the requested type name.
+	\FIXME need scope and function / file
+	\returns true o nsuccess, false if not found
+*/
+bool SymTypeTree::get_type( std::string type_name, SymType **type )
+{
+	for(int i=0; i<m_types.size(); i++)
+	{
+		if(match)
+		{
+			
+			return true;
+		}
+	}
+	return false;
+}
+
+#endif
+
+
+/** Search for the specified type is the supplied context.
+	Starts at closest scope and works out.
+		1) blocks in the current function
+		2) File scope
+		3) Global scope
+
+	\param name		Name of the type to search for.
+	\param context	Context to search for the type
+	\returns 		A pointer to the best matching type entry or NULL if not found
+*/
+SymType *SymTypeTree::get_type( std::string type_name,
+								ContextMgr::Context context )
+{
+	//cout << "looking for '" << type_name << "'" << endl;
+	
+	/// @FIXME: this a a crude hack that just takes the first matching type and ignores the scope requirments
+	for(int i=0; i<m_types.size(); i++)
+	{
+		if( m_types[i]->name()==type_name )
+		{
+			return m_types[i];
+		}
+	}
+	
+	return 0;	// not found
+}
+
+
+std::string SymTypeTree::pretty_print( SymType *ptype,
+							char fmt,
+							uint32_t flat_addr,
+							std::string subpath )
+{
+	
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// SymType
+////////////////////////////////////////////////////////////////////////////////
+
+bool SymType::read_memory( uint32_t flat_addr, uint32_t len, uint8_t *buf )
+{
+	char area;
+	ADDR a = MemRemap::target(flat_addr, area );
+	printf("reading from addr= 0x%04x\n",a);
+	target->read_data( a, len, buf );
+	//memset( buf,0xFF,length );	// hack for test
+	return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// SymTypeChar / SymTypeUChar
+////////////////////////////////////////////////////////////////////////////////
+std::string SymTypeChar::pretty_print( std::string name,
+									  int indent_cnt,
+									  uint32_t &addr )
+{
+	char buf[32];
+	char c;
+	read_memory( addr, 1, (uint8_t*)&c );
+	std::string s;
+	s += indent(indent_cnt);
+	s += name;
+	snprintf(buf,sizeof(buf)," = %i\n", c);
+	s += buf;
+	return s;
+}
+
+std::string SymTypeUChar::pretty_print( std::string name,
+										int indent_cnt,
+										uint32_t &addr )
+{
+	char buf[32];
+	unsigned char c;
+	read_memory( addr, 1, &c );
+	std::string s;
+	s += indent(indent_cnt);
+	s += name;
+	snprintf(buf,sizeof(buf)," = %i\n", c);
+	s += buf;
+	return s;
+}
+
+
+static volatile int g = 42;
+static volatile int ar[] = { 3,6,9,12 };
+typedef struct
+{
+	int m;
+	int j;
+	float x[3];
+	struct 
+	{
+		int p;
+		int q;
+	} zap;
+		
+} TEST;
+static volatile TEST abc;
+std::string SymTypeInt::pretty_print( 	std::string name,
+										int indent_cnt,
+										uint32_t &addr )
+{
+#if 0
+	char buf[32];
+	int16_t i;
+	read_memory( addr, 2, (uint8_t *)buf );
+	i = buf[0] | (buf[1]<<8);
+	
+	std::string s;
+	s += indent(indent_cnt);
+	s += name;
+	snprintf(buf,sizeof(buf)," = %i\n", i);
+	s += buf;
+	return s;	
+#else
+	OutFormat of;
+	std::string s;
+	s += indent(indent_cnt);
+	s += name;
+	s += '=';
+	s += of.print( 'x', addr, 2 );
+	return s;
+#endif
+
+	
+	abc.m = 1;
+	abc.j = 3;
+	abc.x[0] = 7;
+	abc.x[1] = 8;
+	abc.x[2] = 9;
+	
+	abc.zap.p = 4;
+	abc.zap.q = 5;
+	
+	cout <<endl<< abc.m<<endl;
+	cout << ar[0];
+}
+
+std::string SymTypeUInt::pretty_print(	std::string name,
+										int indent_cnt,
+										uint32_t &addr )
+{
+	char buf[32];
+	uint16_t i;
+	read_memory( addr, 2, (uint8_t *)buf );
+	i = buf[0] | (buf[1]<<8);
+	
+	std::string s;
+	s += indent(indent_cnt);
+	s += name;
+	snprintf(buf,sizeof(buf)," = %i\n", name.c_str(), i);
+	s += buf;
+	return s;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// SymTypeStruct
+////////////////////////////////////////////////////////////////////////////////
+
+
+int32_t SymTypeStruct::size()
+{
+	int32_t size=0;
+//	for( int i=0; i<m_members.size(); i++)
+//		size += m_members[i].pType->size();
+	return size;
+}
+
+
+// GDB standard text representation
+std::string SymTypeStruct::text()
+{
+	char buf[255];
+	string s;
+	
+	for(int i=0; i<m_members.size(); i++)
+	{
+		if( m_members[i].count!=1 )
+			snprintf(buf,sizeof(buf),"%s %s[%i]\n",
+					m_members[i].type_name.c_str(),
+					m_members[i].member_name.c_str(),
+					m_members[i].count);
+		else
+			snprintf(buf,sizeof(buf),"%s %s\n",
+					 m_members[i].type_name.c_str(),
+					 m_members[i].member_name.c_str() );
+		s+=buf;
+	}
+	return s;
+}
+
+
+/** Add a member to a structure.
+	\param name		Name of the member to add.
+	\param ptype	pointer to the type to add.
+	\param count	Count of number of items if the member is an array.
+
+	This type only stores the names of the associated types.
+	The lookup for each must be done manually for now.
+*/
+void SymTypeStruct::add_member( std::string member_name,
+								std::string type_name,
+								/*SymType *ptype,*/
+								 uint16_t count )
+{
+	MEMBER m;
+	m.member_name	= member_name;
+	m.type_name		= type_name;
+	m.count			= count;
+	m_members.push_back(m);
+	cout << "adding: member ='"<<member_name<<"', "<<
+			"type = '"<<type_name<<"', "<<
+			" count="<<count<<endl;
+}
